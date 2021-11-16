6ab11f60b0c174f69ade4b91da5aafe37067aefa
AzzaMaalej <maroua.arifa@esprit.tn>
2021-11-17 00:12:34 +0100

    updates doc

--- src/main/java/com/bfi/loanservice/payload/request/SupportingDocRequest.java
+++ src/main/java/com/bfi/loanservice/payload/request/SupportingDocRequest.java
@@ -1,6 +1,9 @@
 package com.bfi.loanservice.payload.request;
 
 import com.bfi.loanservice.models.Customer;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -8,7 +11,9 @@ import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
-
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class SupportingDocRequest {
 
     private Long id;
